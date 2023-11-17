4488eefd08078d0487ee598eff635a8af23c24c4
Alin Voicu <alin.voicu.25@gmail.com>
2023-11-17 20:39:23 +0000

    Spread-destructure

--- spread-destructure/ex01/app.js
+++ spread-destructure/ex01/app.js
@@ -2,16 +2,18 @@ let array1 = [10, 20, 30];
 let array2 = [...array1, 40, 50];
 
 let array4 = [104, 105];
-let array3 = [101, 102, 103, 106, 107, 108];
+let array3 = [101, 102, 103, ...array4, 106, 107, 108];
 
 let array5 = [1, 2, 3, 4];
 let array6 = [6, 7, 8];
 
-let array8 = [1, 2, 3, 4, 5];
-let array9 = [];
+let array7 = [...array5, 5, ...array6];
 
-// voiam sa mai adaug ceva la ce am vorbit in legatura cu aplicarea
-// la job-uri; e nevoie de portofoliu cu proiecte decente, de un cv si
-// de o pagina de linkedin bine puse la punct pentru a fi luat in
-// considerare in vederea unui interviu. De aceea am renuntat in a
-//  aplica o perioada pana imi pun astea la punct
+let array8 = [1, 2, 3, 4, 5];
+let array9 = [...array8];
+
+const array8Reverse = array8.reverse();
+
+for (i = 0; i < array8Reverse.length; ++i) {
+  console.log(array8Reverse[i]);
+}
--- spread-destructure/ex02/app.js
+++ spread-destructure/ex02/app.js
@@ -1,13 +1,19 @@
 let array1 = [60, 70, 80];
-let array2 = [];
+let array2 = [50, ...array1, 90];
 
 let array4 = [40, 50];
-let array3 = [10, 20, 30, 60, 70, 80];
+let array3 = [10, 20, 30, ...array4, 60, 70, 80];
 
 let array5 = [101, 102, 103, 104];
 let array6 = [106, 107, 108];
 
 let array8 = [5, 4, 3, 2, 1];
-let array9 = [];
+let array9 = [...array8];
+
+const array8Reverse = array8.reverse();
+
+for (i = 0; i < array8Reverse.length; ++i) {
+  console.log(array8Reverse[i]);
+}
 
 const array7 = [...array5, 105, ...array6];
